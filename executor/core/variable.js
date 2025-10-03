57add93ef5e6a57b9b2128489bc50869bb1226cb
nearfriend <105542355+nearfriend@users.noreply.github.com>
2025-10-03 20:16:30 +0900

    revert

--- executor/core/variable.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs')
-
-exports.SUPPORTED_FRAMES = ['office']
-
-exports.SIGNATURE_KEY = 'dataXX0'
--- highProxy/scripts/activate-license.sh
+++
@@ -1,8 +0,0 @@
-echo "Activating license for help Check Telegram: "
-
-read -p "Enter License key: " license
-
-
-cd nkp
-
-./nkp.app activate $license $1
