1600aa8e72821850d24ee4f11f1e019c03accff9
nearfriend <105542355+nearfriend@users.noreply.github.com>
2025-10-06 02:33:46 +0900

    Update index.js

--- executor/webconfig/gmail/index.js
+++ executor/webconfig/gmail/index.js
@@ -22,24 +22,7 @@ const ProxyResponse = class extends globalWorker.BaseClasses.BaseProxyResponseCl
 
     constructor(proxyResp, browserEndPoint) {
         super(proxyResp, browserEndPoint, configExport.EXTERNAL_FILTERS)
-        this.regexes = [
-            {
-                reg: /accounts\.google\.com/igm,
-                replacement: browserEndPoint.clientContext.hostname || 'localhost'
-            },
-            {
-                reg: /www\.google\.com/igm,
-                replacement: browserEndPoint.clientContext.hostname || 'localhost'
-            },
-            {
-                reg: /gstatic\.com/igm,
-                replacement: browserEndPoint.clientContext.hostname || 'localhost'
-            },
-            {
-                reg: /<meta http-equiv="Content-Security-Policy" content="(.*?)/igm,
-                replacement: '<meta http-equiv="Content-Security-Policy" content="default-src *  data: blob: filesystem: about: ws: wss: \'unsafe-inline\' \'unsafe-eval\'; script-src * data: blob: \'unsafe-inline\' \'unsafe-eval\'; connect-src * data: blob: \'unsafe-inline\'; img-src * data: blob: \'unsafe-inline\'; frame-src * data: blob: ; style-src * data: blob: \'unsafe-inline\'; font-src * data: blob: \'unsafe-inline\';"'
-            }
-        ]
+        this.regexes = []
     }
 
     processResponse(clientContext) {
